2103487c541cf3cbb9dc33fa72a8f959c703fe61
ig4e <50212311+ig4e@users.noreply.github.com>
2019-06-02 08:55:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`** الله يرحم مصطفى الشبح كان طفل كويس**[ " ${x} " ]`)
+        message.channel.send(`** مصطفى طفلللل عيل علق بيشخ كل يوم**[ " ${x} " ]`)
           .then(m => {
             count++;
           })
